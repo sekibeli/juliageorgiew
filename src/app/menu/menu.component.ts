fc6262f24266e9b6f3320baf33cbcaee25ac60f4
Julia Georgiew <java2011@me.com>
2023-05-27 20:47:38 +0200

    darkmode

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+  @Input() darkMode = true;
+
+  constructor(){}
+  
+  ngOnInit(): void {
+    
+  }
 
 }
